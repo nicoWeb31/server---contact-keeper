7f8299fbd705f21fc47a3df5d2cc1fedc2c5ac34
nicoWeb31 <nico.riot@free.fr>
2020-10-12 19:36:55 +0200

    config plus end point

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(rq,res)=>res.json({message: 'welcome to the contact api'}))
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`server started on port ${PORT}`));
