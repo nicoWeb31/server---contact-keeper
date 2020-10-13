dbeb7490319559436826238215dcba0848f30eab
nicoWeb31 <nico.riot@free.fr>
2020-10-13 15:19:57 +0200

    contact delete and of api

--- route/contact.js
+++ route/contact.js
@@ -65,16 +65,64 @@ router.post('/',[auth,[
 //@desc Update contact
 //@access Private
 
-router.put('/:id',auth,(req,res)=>{
-    res.send('update contact id');
+router.put('/:id',auth,async(req,res)=>{
+    const {name, email,phone, type} = req.body;
+
+    //build contact object
+    const contactField = {};
+
+    if(name) contactField.name = name;
+    if(email) contactField.email = email;
+    if(phone) contactField.phone = phone;
+    if(type) contactField.name = type;
+
+    try{
+
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({message:'Contact not found'})
+
+        //Make sure user owns contact
+        if(contact.user.toString() !== req.user.id){
+            return res.status(401).json({msg : 'Not authorized'})
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            {$set:contactField},
+            {new:true});
+
+        res.json(contact)    
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Errors');
+    }
+
 })
 
 //@route Delete api/contact/:id
 //@desc delete contact
 //@access Private
 
-router.delete('/:id',auth,(req,res)=>{
-    res.send('delete contact id');
+router.delete('/:id',auth,async (req,res)=>{
+    try{
+
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({message:'Contact not found'})
+
+        //Make sure user owns contact
+        if(contact.user.toString() !== req.user.id){
+            return res.status(401).json({msg : 'Not authorized'})
+        }
+
+        await Contact.findByIdAndRemove(req.params.id)
+        res.json({message:'Contact removed'})    
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Errors');
+    }
 })
 
 module.exports = router;
